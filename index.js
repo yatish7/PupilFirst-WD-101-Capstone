8215492ed6170c2802da715d65c9ae336664010c
yatish7 <yatish.manne09@gmail.com>
2023-11-21 21:52:38 +0530

    changed

--- index.js
+++ index.js
@@ -1,35 +1,97 @@
-const dob = document.getElementById('dob');
-        
-dob.addEventListener('input', function() {
-    const entryDate = new Date(dob.value);
-    const maximum = new Date();
-    maximum.setFullYear(maximum.getFullYear() - 18);
-    const minimum = new Date(maximum);
-    minimum.setFullYear(maximum.getFullYear() - 55);
-
-    if (entryDate > maximum || entryDate < minimum) {
-        dob.setCustomValidity('Date should be between 18 and 55 years');
-    } else {
-        dob.setCustomValidity('');
+const dob1 = document.getElementById("dob");
+dob1.addEventListener("change", () => validateDob(dob1));
+function validateDob(dob1){
+let Date2=dob1.value.split("-");
+let year=Date2[0];
+let month=Date2[1];
+let date=Date2[2];
+let birthdate = new Date(year, month, date);
+let today = new Date();
+let currentYear= today.getFullYear();
+let birthYear=birthdate.getFullYear()
+let aging = currentYear - birthYear;
+let monthDiff = today.getMonth() - birthdate.getMonth();
+if ((today.getDate() < birthdate.getDate())||monthDiff<0) 
+{
+aging--;
+}
+if (aging<18 || aging>55) 
+{
+dob1.setCustomValidity("Your age is not in between 18 and 55");
+dob1.reportValidity();
+}
+else
+{
+dob1.setCustomValidity("");
+}
+}
+let email=document.getElementById("email");
+email.addEventListener('input',() => validate(email));
+function validate(element){
+if(element.validity.typeMismatch){
+    element.setCustomValidity("Invalid email");
+    element.reportValidity();
     }
-});
+    else{
+    element.setCustomValidity('');
+    }
+}           
+let userform=document.getElementById('doo');
+const retriveEntries=()=>{
+    let entries=localStorage.getItem("entries");
+    if(entries){
+        entries=JSON.parse(entries);
+    }
+    else{
+        entries=[];
+    }
+    return entries;
+}
+let userEntries=retriveEntries();
 
+const displayEntries=()=>{
+    let entries=retriveEntries();
+    const tableEntries=entries.map((entry)=>{
+    const name=`<td>${entry.name}</td>`;
+    const email=`<td>${entry.email}</td>`;
+    const password=`<td>${entry.password}</td>`;
+    const dob=`<td >${entry.dob}</td>`;
+    const accept=`<td>${entry.acceptedTermsAndCondition}</td>`;
+    const row=`<tr>${name} ${email} ${password} ${dob} ${accept}</tr>`;
+    return row;
+    }).join("\n");
 
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const name = document.querySelector('input[name="name"]').value;
-    const email = document.querySelector('input[name="email"]').value;
-    const password = document.querySelector('input[name="password"]').value;
-    const dob = document.querySelector('input[name="DOB"]').value;
-    const acceptedTerms = document.querySelector('input[name="terms"]').checked ? 'Accepted' : 'Not Accepted';
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
-    localStorage.setItem('dob', dob);
-    localStorage.setItem('acceptedTerms', acceptedTerms);
-    document.getElementById('Name').innerText = name;
-    document.getElementById('Email').innerText = email;
-    document.getElementById('Password').innerText = password;
-    document.getElementById('DOB').innerText = dob;
-    document.getElementById('AcceptedTerms').innerText = acceptedTerms;
-});
+    const table=`<table border="2">
+    <tr>
+    <th>Name</th>
+    <th>Email</th>
+    <th >Password</th>
+    <th>Dob</th>
+    <th>Accepted terms?</th>
+    </tr>
+    ${tableEntries}</table>`;
+    let details=document.getElementById("entries");
+    details.innerHTML=table;
+}
+
+const saveUserForm=(event)=>{
+     event.preventDefault();
+     const name=document.getElementById("name").value;
+     const email=document.getElementById("email").value;
+     const password=document.getElementById("password").value;
+     const dob=document.getElementById("dob").value;
+
+     const acceptedTermsAndCondition=document.getElementById("acceptTerms").checked;
+     const entry={
+        name,
+        email,
+        password,
+        dob,
+        acceptedTermsAndCondition
+     };
+     userEntries.push(entry);
+     localStorage.setItem("entries",JSON.stringify(userEntries));
+     displayEntries();
+}
+userform.addEventListener("submit",saveUserForm);
+displayEntries();
